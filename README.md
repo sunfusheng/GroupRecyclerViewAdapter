674e184caba5c6fe4a96edf1969ebea75539f71d
sfsheng0322 <sfsheng0322@gmail.com>
2018-02-20 15:19:37 +0800

    V1.0.3

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GroupRecyclerViewAdapter [ ![Download](https://api.bintray.com/packages/sfsheng0322/maven/GroupRecyclerViewAdapter/images/download.svg) ](https://bintray.com/sfsheng0322/maven/GroupRecyclerViewAdapter/_latestVersion)
 
-分组列表
+可增删改查、可展开收起的分组列表，轻松帮你实现【微信-我】等类似页面，动态可配置
 
 <br/>
 
@@ -10,7 +10,7 @@
 
 <br/>
 
-#### Gradle:
+### Gradle:
 
     compile 'com.sunfusheng:GroupRecyclerViewAdapter:<latest-version>'
 
--- Sample/src/main/java/com/sunfusheng/adapter/sample/MainActivity.java
+++ Sample/src/main/java/com/sunfusheng/adapter/sample/MainActivity.java
@@ -1,20 +1,32 @@
 package com.sunfusheng.adapter.sample;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.sunfusheng.FirUpdater;
 import com.sunfusheng.adapter.sample.adapter.MainGroupAdapter;
 import com.sunfusheng.adapter.sample.util.GroupData;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String API_TOKEN = "3c57fb226edf7facf821501e4eba08d2";
+    public static final String APP_ID = "5a80151e959d6949f2ecb2e8";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_recycler_view);
+
+        checkVersion();
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -27,6 +39,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(this, item.intentClass));
             }
         });
+    }
+
+    private void checkVersion() {
+        if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) &&
+                PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            new FirUpdater(this, API_TOKEN, APP_ID).checkVersion();
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 100 && grantResults.length > 0) {
+            new FirUpdater(this, API_TOKEN, APP_ID).checkVersion();
+        }
     }
 
 }
